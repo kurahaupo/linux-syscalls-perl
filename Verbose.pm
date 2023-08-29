a468c2ed7b09847995f8f725c3643f7c5042bf6a
Martin D Kealey <martin@kurahaupo.gen.nz>
2023-08-29 13:36:21 +1000

    Lots of documentation improvements; add :set and :offset, document :look, mostly quell internal warnings

--- Verbose.pm
+++ Verbose.pm
@@ -12,28 +12,29 @@ Verbose
 =head1 SYNOPSIS
 
     # main.pl
-    use Verbose \@ARGV;
+    use Verbose qw( :argv :lock );
     use MyModule;
 
     # MyModule.pm
     package MyModule {
-    use Verbose 'vvv';
-    say 'Some basic info'    if v;
-    say 'Some detailed info' if vv;
-    say 'Routine minutiae'   if vvv;
-    print 'Foo debug info:', Dumper(\%Foo)  if vvvv;
-    DEBUG {
+      use Verbose 'vvv';
+
+      say 'Some basic info'    if v;
+      say 'Some detailed info' if vv;
+      say 'Routine minutiae'   if vvv;
+      print 'Foo debug info:', Dumper(\%Foo)  if vvvv;
+      DEBUG {
         print "Some long-winded debug info...\n";
         for my $item ( @list ) {
-            printf "%s=%s\n", $item->name, $item->value;
+          printf "%s=%s\n", $item->name, $item->value;
         }
-    };
+      };
     }
 
     # OtherModule.pm
     package OtherModule {
-    use Verbose 'v';
-    say "This won't be output" if v;
+      use Verbose 'v';
+      say "This won't be output" if v;
     }
 
     # command line
@@ -50,8 +51,17 @@ be controlled from the command line without needing to add any extra logic.
 
 The effective verbosity level for a module is given by
 
-    I<effective_verbosity> = min( I<module_verbosity>,
+    I<effective_verbosity> = max( I<module_verbosity>,
                                   I<global_verbosity> + I<module_offset> )
+
+If the I<effective_verbosity> exceeds the threshold for each C<v> or C<DEBUG>
+statement, the relevant action(s) will be taken.
+
+Module developers are free to choose their own range of verbosity settings, so
+I<module_offset> exists to allow these to be re-aligned so that the single
+I<global_verbosity> setting will normally produce output at similar levels of
+significance from all modules. (This tends to mean that low-level modules
+tend to produce less output that high-level modules.)
 
 The I<module_verbosity> can be set in the module itself, so that its output is
 enabled whenever I<global_verbosity> is high enough. Conversely
@@ -64,7 +74,7 @@ Settings can be obtained
 
 =item *
 
-from C<--debug=C<setting,setting,setting,...>> among the program's command line
+from C<--debug=I<setting,setting,setting,...>> among the program's command line
 arguments; or
 
 =item *
@@ -74,16 +84,17 @@ database.
 
 =item *
 
-from C<use Verbose qw( :set=I<setting,setting,setting,...> );> (for temporary
-settings while developing the program).
+from C<use Verbose qw( :set=I<setting,setting,setting,...> );>
+
+(This is intended for temporary use while developing a module.)
 
 =back
 
-In each case I<settings> is a comma-separated list, where each item is C<I<tag>=I<number>>
-for a per-module setting, or just I<number> for the global setting.
+In each case C<I<setting,setting,setting,...>> is a comma-separated list, where
+each item is either C<I<tag>=I<number>> for a per-module setting, or just
+I<number> for the global setting.
 
-When you invoke your program with C<--debug=I<settings>>, the I<settings>
-are interpreted as a comma-separated list of settings;
+For example, invoking your program as
 
     myprogram --debug=MyModule=5,2
 
@@ -171,19 +182,70 @@ Functionally equivalent to:
 
 (Because DEBUG is a unary function, the trailng semicolon is normally required.)
 
-=head2 Control items
+You can add C<=I<number>> to control the trigger level for C<DEBUG> in the
+current package.
 
-These will normally only be used by main.
+=head2 Control tags for all packages
 
-=head3 set_verbose I<$control_string>
+=head3 :all
+
+Import all exported symbols into your package.
+
+=head3 :debug, :none and :set=
+
+(Intended for debugging only.)
+
+C<:debug> and C<:none> force all C<v> functions in the current package to yield
+C<true> or C<false> respectively, and for C<DEBUG> to have the corresponding
+behaviour.
+
+C<:set=I<setting>,I<setting>,I<setting>,...>
+
+=head2 Control tags for main::
+
+These will normally only be used by C<main::>.
+
+=head3 set_verbose
+
+(C<set_verbose> only needs to be specified if you want to import it into a
+package I<other> than C<main::>. It's automatically exported into C<main::>
+because that's where C<GetOptions> is normally used.)
 
 If setting the verbosity via the command line is not convenient -- say, you
 want to set it based on a configuration file -- then you can call
-C<set_verbose> directly.
+C<set_verbose I<$control_string>> directly.
 
 The control string is in the same format as for the C<--debug> command line
 option.  Obviously you want to do this fairly early in the execution of your
 program, as it will remain silent until then.
+
+=head3 :argv
+
+Find, process, and remove any C<--debug=I<string>> parameters in C<@ARGV>.
+Combined with C<:lock>, this enables the C<v> functions to be reduced to
+constants, allowing Perl's compilation phase to perform constant folding and
+other optimisations.
+
+Importing :argv will only cause this to be done once, even if it occurs in
+multiple C<import> statements. If you need to process C<@ARGV> separately for
+each C<import> statement, simply pass an array ref C<\@ARGV> instead of
+C<:argv>.
+
+The array is modified in-place, so that C<--debug=...> is not present when
+later processed by C<GetOptions>.
+
+(This method can be used to process arrays other than C<@ARGV>. However it is
+difficult to envisage a situation where this would be useful, so if you have a
+practical example using this, please contact this module's maintainer.)
+
+=head3 :lock
+
+Prohibit further calls to C<set_verbose>, enabling optimisations that assume
+that verbosity levels cannot change. Useful in conjunction with C<:argv>.
+
+Do not use this option if you may want to change the verbosity settings while
+the program is running; for example, if they can be read from a configuration
+file.
 
 =head3 :nocompat
 
@@ -196,8 +258,8 @@ produce a compile-time error).
 This package was originally written by Martin Kealey in 2003 and released under
 the GNU Public Licence version 2.
 
-This help section was added by Ihug in 2008, replacing a previous stub help
-section written in 2004.
+This help section was completely rewritten by Martin Kealey between 2016 and
+2023, replacing a previous help section written by Ihug in 2008.
 
 The :nocompat mode was actually implemented by Martin Kealey in 2019; also
 improved the efficiency of the test functions by having the "global level"
@@ -216,8 +278,9 @@ my %level_for;
 my %offset_for;
 my $global_level = 0;
 
-my $seen_nocompat;      # set to 1 when :no_compat is imported
+my $seen_nocompat;      # set to 1 when :nocompat is imported
 my $seen_compat;        # set to module name that last used compat mode
+my $locked;             # set_verbose no longer allowed
 
 sub steal_argv_debug(;\@);
 
@@ -228,11 +291,36 @@ sub _croak_or_die {
     die $e;
 }
 
-    { package Verbose::Errors::; }
+package Verbose::Errors:: {
+    use overload '""' => sub { return $_[0]->{msg} };
+}
 
+my $have_stolen_argv;
+
+# GetOptions can be called in 3 ways:
+# + by user code, with 1 parameter: the option value (enforced by the prototype)
+# + by GetOptions, with 2 parameters: the option name, and the option value
+# + during importing, with 3 or 4 parameters: a reason, an optional caller
+#   depth, the option name, and the option value.
+# (The option name and reason are only used for error reporting)
 sub set_verbose($) {
-    my $o = $_[-1];     # GetOptions calls us with two args, and the last is the interesting one
-    my @o = split ',', $o;
+    my $arg = pop;
+    if ($locked) {
+        my ($opkg, $ofilename, $olineno) = @$locked;
+        my  $opt = pop;     # Arg name that caused set_verbose to be invoked
+        my  $why = shift    # General reason why we were called
+                 // ($opt && 'GetOpts')
+                 // 'Direct';
+        my $depth = pop // ( $why eq 'Stealing' ? 3 : $why eq 'Using' ? 2 : 1 );
+        die bless {
+            why     => 'TooLate'.$why,
+            option  => $opt,
+            arg     => $arg,
+            msg     => "Cannot change verbosity after lock was set at line $olineno in $ofilename",
+            where   => [caller $depth+1],
+        }, Verbose::Errors::;
+    }
+    my @o = split ',', $arg;
     for my $o (@o) {
         if ( $o =~ /=/ ) {
             $level_for{$`} = $';
@@ -241,15 +329,24 @@ sub set_verbose($) {
             for my $k ( keys %level_for ) {
                 $level_for{$k} = $o - $offset_for{$k};
             }
-        } elsif ( @_ == 2 ) {
-            # we were called from GetOptions
-            die "Invalid value $o from command-line parameter $_[0]\n";
-        } elsif ( @_ == 3 ) {
-            # we were called from import (and thence 'use' or 'require')
-            die "Invalid value $o while importing into $_[1] with :set=\n";
         } else {
-            warn "invalid value $o";
-            die bless { type => "BADPARAM", value => $o }, Verbose::Errors::;
+            my  $opt = pop;     # Arg name that caused set_verbose to be invoked
+            my  $why = shift;   # General reason why we were called
+                $why //= $opt && 'GetOpts' // 'Direct';
+            my  $msg = "Invalid value '$o'";
+                $msg .= " as part of '$arg'" if $o ne $arg;
+                $msg .= " for option $opt" if $opt;
+                $msg .= " ($why)";
+            my $depth = pop // ( $why eq 'Stealing' ? 3 : $why eq 'Using' ? 2 : 1 );
+            die bless {
+                why     => 'Bad'.$why,
+                option  => $opt,
+                arg     => $arg,
+                values  => \@o,
+                value   => $o,
+                msg     => $msg,
+                where   => [caller $depth+1],
+            }, Verbose::Errors::;
         }
     }
 }
@@ -274,11 +371,13 @@ sub import {
     my $override;
     my $want_compat;
 
-    warn "import: args=[@_]";
+    my $will_lock;  # delay locking until all args are processed
+
+    warn "import: args=[@_]" if $^C;
     for (@_) {
         if    ( ref($_) eq 'ARRAY'      ) { steal_argv_debug @$_ }
         elsif ( $_ eq ':all'            ) { $export_debug = $export_sv = 1; $export_v ||= 1 }
-        elsif ( $_ eq ':argv'           ) { steal_argv_debug }
+        elsif ( $_ eq ':argv'           ) { steal_argv_debug if ! $have_stolen_argv++ }
         elsif ( $_ eq 'DEBUG'           ) { $export_debug = 1 }
         elsif (      /^DEBUG=(\d+)$/    ) { $export_debug = 1; $debug_level = $1 }
         elsif ( $_ eq ':debug'          ) { $override = \&vdebug; $debug_level = 0; }
@@ -287,29 +386,31 @@ sub import {
         elsif ( $_ eq ':silent'         ) { $override = \&vsilent }
         elsif ( $_ eq ':nocompat'       ) { $want_compat = 0; $seen_nocompat ||= "$pkg explicitly"; }
         elsif ( $_ eq ':compat'         ) { $want_compat = 1 }
-        elsif ( /^=|^:set=/             ) { &set_verbose(undef, $pkg, $') }
-        elsif ( /^v+$/                  ) { $export_v = length $_ }
-        elsif ( /^[-+]?\d+$/            ) { $offset_from_global = 0+$_ }
-        elsif ( /^[A-Za-z]\w+(::\w+)*$/ ) { $tag = $_ }
+        elsif (      /^=|^:set=/        ) { &set_verbose('Using', 1, $pkg, $') }
+        elsif (      /^v+$/             ) { $export_v = length $_ }
+        elsif (      /^:offset=[-+]?\d+$/ ) { $offset_from_global = 0+$_ }
+        elsif (      /^\w+(::\w+)*$/    ) { $tag = $_ }
         else { _croak_or_die "Don't understand $_"; }
     }
+
+    $locked = [caller] if $will_lock;
 
     if ($tag) {
         $tag =~ s/::$//;
         $offset_for{$tag} = $offset_from_global if ! exists $offset_for{$tag};
         my $new_level = $global_level - $offset_from_global;
         if ( ! exists $level_for{$tag} || $new_level < $level_for{$tag} ) {
-            warn "import: Setting tag:$tag to $new_level";
+            warn "import: Setting tag:$tag to $new_level" if $^C;
             $level_for{$tag} = $new_level;
         } else {
-            warn "import: Leaving tag:$tag at $level_for{$tag}";
+            warn "import: Leaving tag:$tag at $level_for{$tag}" if $^C;
         }
         $tag = \$level_for{$tag};
     } else {
         if ($offset_from_global) {
             _croak_or_die "Can't have an offset from the global level without having a tag";
         }
-        warn "import: Using global tag with $global_level";
+        warn "import: Using global tag with $global_level" if $^C;
         $tag = \$global_level;
     }
 
@@ -358,17 +459,21 @@ sub import {
         my $val = $exports{$sym};
         local $SIG{__DIE__} = sub {
             warn $@;
-            printf STDERR "pkg=%s\n", $pkg;
-            printf STDERR "rpkg=%s\n", $rpkg;
-            printf STDERR "sym=%s\n", $sym;
-            printf STDERR "val=%s\n", $val;
-            printf STDERR "rpkg{sym}=%s\n", $rpkg->{$sym} // '(undef)';
+            printf STDERR "pkg=%s\n"
+                        . "rpkg=%s\n"
+                        . "sym=%s\n"
+                        . "val=%s\n"
+                        . "rpkg{sym}=%s\n",
+                    $pkg, $rpkg, $sym, $val,
+                    $rpkg->{$sym} // '(undef)';
+            $SIG{__DIE__} = undef;  # just in case a DESTROY dies on the way out
             exit 1;
         };
         if ($rpkg->{$sym}) {
             if ( ! ref $val ) {
                 # Equivalent to "use constant", but pessimized
-                $val = sub { $val };
+                my $xval = $val;
+                $val = sub { $xval };
             }
             # typeglob slot automatically selected by ref of $val; any of
             # SCALAR, ARRAY, HASH, CODE, IO, and FMT.
@@ -390,11 +495,10 @@ sub import {
 sub steal_argv_debug(;\@) {
     my ($argv) = @_;
     $argv ||= \@::ARGV;
-    warn "Stealing from $argv";
     my $nomore = 0;
     @$argv = grep {
                     if ( ! $nomore && /^--debug=/ ) {
-                        set_verbose $';
+                        &set_verbose( 'Stealing', 2, 'debug', $' );
                         0;
                     } else {
                         ( $_ eq '--' || ! /^-/ ) and ++$nomore;
@@ -415,9 +519,30 @@ sub showguts() {
 
 1;
 
-=head1 BUGS
+=head1 BUGS & LIMITATIONS
 
-Plans:
+=over 4
+
+=item
+
+This module uses its own hand-crafter exporter, because it needs to change what
+gets exported when a given symbol is asked for. This means it needs to mimic
+some rather complex parts of the standard C<Exporter> module, and in
+particular, exporting scalar constants requires some in-depth knowledge of how
+symbol tables work. If C<Exporter> ever changes, this module is likely to
+break.
+
+=item
+
+Used as intended this module should be straightforward, but if you start trying
+to do tricky things you will quickly find yourself having to deal with the
+interplay between the BEGIN and RUN phases. I've tried to document the
+important aspects of that here, but no doubt I've missed some subtler points
+that you'll need to discover for yourself.
+
+=back
+
+=head1 PLANS & TODO
 
 =over 4
 
@@ -425,15 +550,7 @@ Plans:
 
 Make functionality match description. Some stuff doesn't work quite right.
 
-=item
-
-Change handling of a "global" level so that the "v" functions no longer have to
-check it, they just check their respective levels, while set_verbose sets all
-levels to the global level (except those which are set explicitly).
-
-=item
-
-Convert level comparisons into flag look-ups, which should be faster.
+(Getting there.)
 
 =back
 
